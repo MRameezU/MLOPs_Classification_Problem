63bda7130b20392f03843af450342d43fedb11e9
MRameezU <urameez@outlook.com>
2024-09-27 04:09:33 +0500

    added utilities

--- us_visa/utils/main_utils.py
+++ us_visa/utils/main_utils.py
@@ -9,7 +9,7 @@ from pandas import DataFrame
 from us_visa.exception import USvisaException
 from us_visa.logger import logging
 
-def read_yaml_fiel(file_path:str):
+def read_yaml_file(file_path:str):
     try:
         with open(file=file_path,mode="rb") as yaml_file:
             return yaml.safe_load(yaml_file)
@@ -27,5 +27,55 @@ def write_yaml_file(file_path,content,replace:bool=False):
     except Exception as e:
         raise USvisaException(e,sys) from e
 
+
+def save_numpy_array_date(file_path,array:np.array):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file=file_path,mode="wb") as file_obj:
+            np.save(file_obj,array)
+
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+
+
+def load_numpy_array_date(file_path):
+    try:
+        with open(file=file_path,mode='rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise USvisaException(e,sys) from e
+
 def load_object(file_path):
-    ...
+    logging.info("Entered the load_object method of utils")
+    try:
+        with open(file=file_path,mode='rb') as file_obj:
+            obj=dill.load(file_obj)
+
+        logging.info("Exited the load_object method of utils")
+        return obj
+    except Exception as e:
+        raise USvisaException(e,sys) from e
+
+
+def save_object(file_path,obj):
+    logging.info("Entered the save_object method of utils")
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file=file_path, mode='wb') as file_obj:
+            dill.dump(obj,file_obj)
+
+        logging.info("Exited the save_object method of utils")
+        return obj
+    except Exception as e:
+        raise USvisaException(e, sys) from e
+
+
+def drop_columns(df:DataFrame,cols):
+    logging.info("Entered the drop_columns method of utils")
+    try:
+        df=df.drop(columns=cols,axis=1)
+        logging.info("Exited the drop_columns method of utils")
+        return df
+    except Exception as e:
+        raise USvisaException(e, sys) from e
