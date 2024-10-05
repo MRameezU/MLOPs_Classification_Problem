1e3e46443554db74f3975a29276ac8a15e4e21e5
MRameezU <urameez@outlook.com>
2024-10-05 10:25:14 +0500

    missing variables added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -14,16 +14,19 @@ COLLECTION_NAME=os.getenv("COLLECTION_NAME")
 MONGODB_URL_KEY=os.getenv("CONNECTION_URL") # connection string
 
 
-PIPELINE_NAME:str = "usvisa"
-ARTIFACT_DIR:str = "artifact"
+PIPELINE_NAME= "usvisa"
+ARTIFACT_DIR= "artifact"
 
 FILE_NAME="usvisa.csv"
+TRAIN_FILE_NAME="train.csv"
+TEST_FILE_NAME="test.csv"
+
 MODEL_FILE_NAME="model.pkl"
 
 # Data Ingestion constant starting with DATA_INGESTION VAR NAME
 
-DATA_INGESTION_COLLECTION_NAME:str = "visa_data"
-DATA_INGESTION_DIR_NAME:str="data_ingestion"
-DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
-DATA_INGESTION_INGESTED_DIR:str="ingested"
+DATA_INGESTION_COLLECTION_NAME= "visa_data"
+DATA_INGESTION_DIR_NAME="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR= "feature_store"
+DATA_INGESTION_INGESTED_DIR="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2 # 80% training and 20% validation
