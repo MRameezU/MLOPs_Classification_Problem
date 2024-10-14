caf1e33f1d369c7e55e6c8326ad1d30576b12b89
MRameezU <urameez@outlook.com>
2024-10-14 23:46:21 +0500

    tested training pipeline

--- demo.py
+++ demo.py
@@ -1,9 +1,15 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-# logging.info("Custom logging test")
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+# # logging.info("Custom logging test")
+#
+# try:
+#     error=1/0
+# except Exception as e:
+#     raise USvisaException(e,sys)
 
-try:
-    error=1/0
-except Exception as e:
-    raise USvisaException(e,sys)
+# training pipeline test
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+object = TrainPipeline()
+object.run_pipeline()
