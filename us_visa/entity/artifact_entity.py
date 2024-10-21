1d40fb085e87b706022a97ed507943ec0a79838b
MRameezU <urameez@outlook.com>
2024-10-21 23:36:45 +0500

    Add type annotations

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -9,5 +9,5 @@ class DataIngestionArtifact:
 @dataclass
 class DataValidationArtifact:
     validation_status:bool
-    message:str
-    drift_report_file_path:str
+    message: str
+    drift_report_file_path: str
