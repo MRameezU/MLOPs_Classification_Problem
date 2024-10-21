c310ae876ed55e41d1dee7e2bfa437ad2fb70c20
MRameezU <urameez@outlook.com>
2024-10-21 23:38:29 +0500

    Add type annotations and removed unused imports

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -1,4 +1,4 @@
-import os
+
 from us_visa.constants import *
 from dataclasses import dataclass
 from datetime import datetime
@@ -44,7 +44,7 @@ class DataIngestionConfig:
 
 @dataclass
 class DataValidationConfig:
-    data_validation_dir= os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
-    drift_report_file_path= os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+    drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
 
